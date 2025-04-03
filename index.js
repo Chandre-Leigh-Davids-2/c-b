46c440d059ebba4d0d9c62b9c3c7349ece115c99
Chandre-Leigh-Davids-2 <chandreleighdavids.17@gmail.com>
2025-04-03 10:58:53 +0200

    edit

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const app = express()
 
 app.use('/sanrio',router)
 
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
+
 
 app.listen(port,()=>{
     console.log('http://localhost:'+ port);
